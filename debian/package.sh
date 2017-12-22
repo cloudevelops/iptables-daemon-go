a7ac8e29473b1b204d9f368d288934db9dadeebf
Zdenek Janda <zdenek.janda@cloudevelops.com>
2017-12-22 15:04:53 +0100

    patched config error

--- debian/package.sh
+++ debian/package.sh
@@ -5,5 +5,5 @@ rm -rf /tmp/iptables-daemon-go
 mkdir -p /tmp/iptables-daemon-go/usr/sbin
 cp -a debian/root/* /tmp/iptables-daemon-go/
 cp -a $GOPATH/bin/iptables-daemon-go /tmp/iptables-daemon-go/usr/sbin
-fpm -s dir -t deb -C /tmp/iptables-daemon-go --name iptables-daemon-go --version 0.0.1 --iteration 1 --description "iptables management in go" --package /tmp/iptables-daemon-go
+fpm -s dir -t deb -C /tmp/iptables-daemon-go --name iptables-daemon-go --version 0.0.1 --iteration 2 --description "iptables management in go" --package /tmp/iptables-daemon-go
 
